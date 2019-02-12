e0da370d26b2dae1af86ff6cb43843cf1991ab9c
rookies <rookies@D-431301404.wipro.com>
2019-02-12 09:35:23 -0200

    first_commit

---
+++ projects/blockchain/src/lib/blockchain.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-blockchain',
+  template: `
+    <p>
+      blockchain works!
+    </p>
+  `,
+  styles: []
+})
+export class BlockchainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ projects/blockchain/src/lib/models/blockchain.ts
@@ -0,0 +1,10 @@
+import { Block } from './block';
+import { Transaction } from './transaction';
+
+export class Blockchain {
+    public chain: Block[];
+    public pendingTransactions: Transaction[] = [];
+    public nodeUrl: string;
+    public networkNodes: string[] = [];
+}
+
